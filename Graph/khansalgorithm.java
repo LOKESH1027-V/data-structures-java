31942fea0778f225046313c431a8820d811addc9
loki <lokesh271003@gmail.com>
2026-07-21 10:09:26 +0530

    Khans alogorithm

---
+++ Graph/khansalgorithm.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class khansalgorithm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i <n; i++) {
+
+            System.out.print("Node: ");
+            int node = sc.nextInt();
+
+            System.out.print("Enter number of adjacent nodes: ");
+            int m = sc.nextInt();
+
+            ArrayList<Integer> cur = new ArrayList<>();
+
+            System.out.println("Enter adjacent nodes:");
+            for (int j = 0; j <m; j++) {
+                int adjNode = sc.nextInt();
+                cur.add(adjNode);
+            }
+
+            while (adj.size() <= node) {
+                adj.add(new ArrayList<>());
+            }
+
+            adj.set(node, cur);
+        }
+        for(int i:topologicalsort(n, adj)){
+            System.out.print(i+" ");
+        }
+    }
+
+    public static int[] topologicalsort(int v,ArrayList<ArrayList<Integer>> adj){
+        int indegree[]=new int[v];
+        for(int i=0;i<v;i++){
+            for(int it:adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<v;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        int top[]=new int[v];
+        int i=0;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            top[i++]=node;
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+        return top;
+    }
+}
